7e79fccad09b00a74396029bebffbef84014062a
pingping.zhang <pingping.zhang@vistel.cn>
2020-07-10 18:26:24 +0800

    add

--- 数据结构的字符串/字符串.py
+++ 数据结构的字符串/字符串.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 # 替换空格为%20
 def replaceSpace(s):
     if not s:
@@ -9,37 +8,35 @@ def replaceSpace(s):
 print(replaceSpace('增长 '))
 
 # 取出字符流中第一个不重复的字符
-# 'google'
-#
-# def first_not_repeating_char(string):
-#     if not string:
-#         return
-#     temp = dict([(x, 0) for x in range(26)])
-#     for s in string:
-#         # ord('a') == 97
-#         temp[ord(s) - 97] += 1
-#     for s in string:
-#         if temp[ord(s) - 97] == 1:
-#             res = s
-#             break
-#     return res
-
-def first_not_repeating_char(string):
+def first_not_repeating_char(string, n):
     if not string:
         return
-    temp = dict([(x, 0) for x in range(26)])
-    for s in string:
-        # ord('a') == 97
-        temp[ord(s) - 97] += 1
-    for s in string:
-        if temp[ord(s) - 97] == 1:
-            res = s
-            break
-    return res
+    dict_count = {i: string.count(i) for i in string}
+    res = []
+    for k, v in dict_count.items():
+        if v == 1:
+            res.append(k)
+    return res[n]
+print(first_not_repeating_char('goole', 0))
 
-first_not_repeating_char('goole')
+# 左旋转字符串S=”abcXYZdef”,要求输出循环左移3位后的结果，即“XYZdefabc”
+def left_rotate_string(string, n):
+    if not string:
+        return
+    return string[n:len(string)] + string[0:n]
 
-a = 1
+print(left_rotate_string('abcXYZdef', 3))
+print(left_rotate_string('ab', 3))
+
+
+
+
+
+
+
+
+
+
 
 
 
